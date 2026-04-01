925a3fe36c8caf8f02c931b5fc0b2d2ed9b3d80c
RyanPowell871 <51717578+RyanPowell871@users.noreply.github.com>
2026-04-01 14:15:04 -0600

    Draft changes via Code Editor

--- src/components/league-info/MissionStatementPage.tsx
+++ src/components/league-info/MissionStatementPage.tsx
@@ -54,7 +54,7 @@ export function MissionStatementPage() {
           <ValueCard
             icon={<Star className="w-5 h-5" />}
             title="Pursuit of Excellence"
-            description="We provide continual participant development opportunities for athletes striving for excellence at every level of competition. TEST2"
+            description="We provide continual participant development opportunities for athletes striving for excellence at every level of competition."
             color="blue"
           />
           <ValueCard
